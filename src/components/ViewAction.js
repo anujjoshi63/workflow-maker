c943eff47f15f39de55d7b83b8a9205849537e06
Anuj <anujjoshi63@gmail.com>
2021-09-22 18:04:39 +0530

    enable node connections

--- src/components/ViewAction.js
+++ src/components/ViewAction.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from '@mui/material';
 import { useHistory } from 'react-router';
@@ -8,11 +8,24 @@ import ReactFlow, {
 	Controls,
 	Background
 } from 'react-flow-renderer';
+
 const ViewAction = () => {
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	const history = useHistory();
 	const elements = useSelector(state => state);
-
+	const onConnect = useCallback(params => {
+		console.log(params);
+		dispatch({
+			type: 'add_data',
+			payload: {
+				id: 'e' + params.source + '-' + params.target,
+				source: params.source,
+				target: params.target,
+				animated: true,
+				style: { stroke: 'black' }
+			}
+		});
+	}, []);
 	return (
 		<>
 			<div>
@@ -24,7 +37,7 @@ const ViewAction = () => {
 				</Button>
 			</div>
 			<div style={{ height: 500, border: '1px solid black' }}>
-				<ReactFlow elements={elements} />
+				<ReactFlow elements={elements} onConnect={onConnect} />
 			</div>
 		</>
 	);
